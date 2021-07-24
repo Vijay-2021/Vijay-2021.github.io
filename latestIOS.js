de84fc213fc6a3bfd66280b4ce74446615ef3739
Vijay-2021 <42916674+Vijay-2021@users.noreply.github.com>
2021-07-24 14:25:24 -0400

    adding files to save latest working ios test
    going to setup a file that saves the last working ios code, and then slowly try to get that to match my current tfjs cod

---
+++ latestIOS.js
@@ -0,0 +1,64 @@
+alert('tfjs test numberr four')
+const videoElement = document.getElementsByClassName('input_video')[0];
+const canvasElement = document.getElementsByClassName('output_canvas')[0];
+const canvasCtx = canvasElement.getContext('2d');
+const FPSElement = document.getElementById('fps');
+
+
+const model = poseDetection.SupportedModels.BlazePose;
+const detectorConfig = {
+  runtime: 'tfjs',
+  enableSmoothing: true,
+  modelType: 'full'
+};
+const flagConfig = {
+    WEBGL_VERSION: 1,
+    WASM_HAS_SIMD_SUPPORT: false,
+    WASM_HAS_MULTITHREAD_SUPPORT: false,
+    WEBGL_CPU_FORWARD:true,
+    WEBGL_PACK: true,
+    WEBGL_FORCE_F16_TEXTURES: true,
+    WEBGL_RENDER_FLOAT32_CAPABLE: false,
+    WEBGL_FLUSH_THRESHOLD: -1,
+    CHECK_COMPUTATION_FOR_ERRORS: false,
+  }
+async function setBackendAndEnvFlags(flagConfig) {
+    if (flagConfig == null) {
+      return;
+    } else if (typeof flagConfig !== 'object') {
+      throw new Error(`An object is expected, while a(n) ${typeof flagConfig} is found.`);
+    } // Check the validation of flags and values.
+    try{
+        tf.env().setFlags(flagConfig);
+    }catch(error){
+        alert("set flags broke");
+    }
+}
+
+
+async function loadModel(){
+    await setBackendAndEnvFlags(flagConfig)
+    try{
+    detector = await poseDetection.createDetector(model, detectorConfig);
+    alert("model built sucessfulLY? ")
+    }catch{
+        alert("error?")
+    }
+    //start the camera after we have confitured the backend and loaded the detector 
+    camera.start()
+}
+const estimationConfig = {flipHorizontal: true};
+const timestamp = performance.now();
+loadModel()
+
+const camera = new Camera(videoElement, {
+    onFrame: async () => {
+
+        canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+        canvasCtx.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
+        var poses = await detector.estimatePoses(videoElement, estimationConfig, timestamp);
+        
+        //alert("yay we are running :) ")
+        
+    }
+});

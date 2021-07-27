08508767ed74e75649b7af2e40edc2158ae154dc
Vijay-2021 <42916674+Vijay-2021@users.noreply.github.com>
2021-07-27 00:21:39 -0400

    adding some prints

--- loadAndroid.js
+++ loadAndroid.js
@@ -16,6 +16,11 @@ function loadAndroid(){
         onFrame: async () => {
             //canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
             //canvasCtx.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
+            alert("this is running, this is a good sing")
+            alert(videoElement.width)
+            alert(canvasElement.width)
+            alert(videoElement.height)
+            alert(canvasElement.height)
             await pose.send({ image: videoElement });
             if(!sentResizedMessage){
                 console.log("Message: resize video");
@@ -24,5 +29,4 @@ function loadAndroid(){
         }
     });
     camera.start()
-    setupCamera()
 }

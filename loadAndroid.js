83977109e56d82371a7c8dc4c6a07d4033c941e8
Vijay-2021 <42916674+Vijay-2021@users.noreply.github.com>
2021-07-28 11:31:09 -0400

    forgot to update android :)

--- loadAndroid.js
+++ loadAndroid.js
@@ -3,6 +3,7 @@ function setLandMarksAndroid(results){
     canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
     
     if(results!=null&&results.poseLandmarks!=null&&results.poseLandmarks.length>0){
+        results.poseLandmarks = createAdditionalJoints(results.poseLandmarks);
         for(var i=0; i < results.poseLandmarks.length;i++){
             results.poseLandmarks[i].x = results.poseLandmarks[i].x * canvasElement.width;
             results.poseLandmarks[i].y = results.poseLandmarks[i].y * canvasElement.height;

766f3be8496e34d164ec707e62c60c325dd28b56
Vijay-2021 <42916674+Vijay-2021@users.noreply.github.com>
2021-07-30 14:42:08 -0400

    testing canvas instead of video element

--- loadAndroidTest.js
+++ loadAndroidTest.js
@@ -1,47 +1,42 @@
-var lastFrameTime = 0 
-
 function setLandMarksAndroid(results){
-    canvasCtx.clearRect(0, 0, videoElement.width, videoElement.height);
-    canvasCtx.drawImage(videoElement, 0, 0, videoElement.width, videoElement.height);
-    console.log("pose landmarks below: ")
-    console.log(results.poseLandmarks)
-    var pixelData = canvasCtx.getImageData(100, 100, 1, 1).data;
-    console.log(pixelData)
-    alert(results.poseLandmarks.length)
-    //updateScreen(results)
+   // canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+   // canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+    
+    if(results!=null&&results.poseLandmarks!=null&&results.poseLandmarks.length>0){
+        results.poseLandmarks = createAdditionalJoints(results.poseLandmarks);
+        var pixelData = canvasCtx.getImageData(100, 100, 1, 1).data;
+        console.log(pixelData)
+        for(var i=0; i < results.poseLandmarks.length;i++){
+            results.poseLandmarks[i].x = results.poseLandmarks[i].x * canvasElement.width;
+            results.poseLandmarks[i].y = results.poseLandmarks[i].y * canvasElement.height;
+        }
+        updateScreen(results.poseLandmarks)
+    }
 }
 
-async function updateVideoAndroid(){
-    alert("frames are being processed")
-    window.requestAnimationFrame(function(){nextFrame()})
-}
-
-function nextFrame(){
-    videoElement.paused||videoElement.currentTime===lastFrameTime||(lastFrameTime=videoElement.currentTime);
-    onFrameAndroid().then(function(){updateVideoAndroid()}) //so  if b exists, then use b.then, else just do q(a). and b is the onframe method, so we run b, then we call the funciton again! okay!
-}
-
-async function onFrameAndroid(){
-    await pose.send({image: videoElement});
-}
-async function loadAndroid(){
+function loadAndroid(){
 
     pose.setOptions({
         modelComplexity: 1,
         smoothLandmarks: true,
         minDetectionConfidence: 0.5,
-        minTrackingConfidence: 0.75
+        minTrackingConfidence: 0.5
     });
-    alert("pose has been loaded")
-    await setupCamera()
-    alert("camera has been setup")
-    videoElement.onloadeddata = async function() {
-        updateVideoAndroid()
-        if(!sentResizedMessage){
-            console.log("Message: resize video");
-            sentResizedMessage = true;
-        }
-    }
-    pose.onResults(setLandMarksAndroid);
 
+    pose.onResults(setLandMarksAndroid);
+    
+    var sentResizedMessage = false;
+
+    const camera = new Camera(videoElement, {
+        onFrame: async () => {
+            canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+            canvasCtx.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
+            await pose.send({ image: canvasElement });
+            if(!sentResizedMessage){
+                console.log("Message: resize video");
+                sentResizedMessage = true;
+            }
+        }
+    });
+    camera.start()
 }

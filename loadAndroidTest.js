f1f86d058b2013608fb533b93ec56cec4dd4561f
Vijay-2021 <42916674+Vijay-2021@users.noreply.github.com>
2021-08-03 13:23:10 -0400

    changed timestamp checking from mediapipe version

--- loadAndroidTest.js
+++ loadAndroidTest.js
@@ -22,8 +22,11 @@ async function updateVideoAndroid(){
 }
 
 function nextFrame(){
-    videoElement.paused||videoElement.currentTime===lastFrameTime||(lastFrameTime=videoElement.currentTime);
-    onFrameAndroid().then(function(){updateVideoAndroid()}) //so  if b exists, then use b.then, else just do q(a). and b is the onframe method, so we run b, then we call the funciton again! okay!
+    console.log(videoElement.currentTime)
+    if(videoElement.currentTime!=lastFrameTime){
+        lastFrameTime=videoElement.currentTime; 
+        onFrameAndroid().then(function(){updateVideoAndroid()})
+    } //so  if b exists, then use b.then, else just do q(a). and b is the onframe method, so we run b, then we call the funciton again! okay!
 }
 
 async function onFrameAndroid(){

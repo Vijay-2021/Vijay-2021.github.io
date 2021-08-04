91f19703a4a4d8c074174794660601a49a542e7a
Vijay-2021 <42916674+Vijay-2021@users.noreply.github.com>
2021-08-04 11:24:57 -0400

    Update chooseTestsPose04.js

--- chooseTestsPose04.js
+++ chooseTestsPose04.js
@@ -93,7 +93,7 @@ async function updateVideo(){
 function nextFrame(){
     var frameUpdate = null;
     videoElement.paused||videoElement.currentTime===lastFrameTime||(lastFrameTime=videoElement.currentTime,frameUpdate=onFrame());
-    frameUpdate?frameUpdate.then(function(){updateVideo()}):updateVideo()
+    updateVideo().then(function(){onFrame()})
 }
 
 async function onFrame(){
